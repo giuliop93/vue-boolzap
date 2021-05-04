7c46e6636741751cc2137fb0a250b8462d6a383b
Giulio_Pa <giuliop93@hotmail.it>
2021-05-04 16:47:58 +0200

    wip

---
+++ data.js
@@ -0,0 +1,135 @@
+Contacts: [
+    {
+        name: 'Lucrezia',
+        avatar: '1',
+        messages: [
+            {
+                message: 'Usciamo stasera?',
+                status: 'sent'
+            },
+            {
+                message: 'Ricordati di me',
+                status: 'sent'
+            },
+            {
+                message: 'Ci penso io',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Carolina',
+        avatar: '2',
+        messages: [
+            {
+                message: 'Ciao come stai?',
+                status: 'sent'
+            },
+            {
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+            },
+            {
+                message: 'Purtroppo domani non posso.',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Florian',
+        avatar: '3',
+        messages: [
+            {
+                message: 'Potete usare un flex grow',
+                status: 'received'
+            },
+            {
+                message: 'Lo potete fare responsive?',
+                status: 'sent'
+            },
+            {
+                message: 'A domani!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Eva',
+        avatar: '4',
+        messages: [
+            {
+                message: 'Lo sai che sei molto carino?',
+                status: 'sent'
+            },
+            {
+                message: 'Si, ma preferirei andare a teatro',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Sofia',
+        avatar: '5',
+        messages: [
+            {
+                message: 'Ricordati di uscire il cane',
+                status: 'sent'
+            },
+            {
+                message: 'Ok, stasera la chiamo',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Flavio',
+        avatar: '6',
+        messages: [
+            {
+                message: 'Ciao, novità?',
+                status: 'sent'
+            },
+            {
+                message: 'Non ancora',
+                status: 'received'
+            },
+            {
+                message: 'Va bene',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Francesco',
+        avatar: '7',
+        messages: [
+            {
+                message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                status: 'sent'
+            },
+            {
+                message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Valentina',
+        avatar: '8',
+        messages: [
+            {
+                message: 'Ciao, andiamo allo stadio?',
+                status: 'received'
+            },
+            {
+                message: 'Forza Real!',
+                status: 'sent'
+            },
+            {
+                message: 'OK!!',
+                status: 'received'
+            }
+        ],
+    }
+]
+
