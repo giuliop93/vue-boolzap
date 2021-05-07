cd86cdf08cd5b8a206d0464b122bb84927b2271b
Giulio_Pa <giuliop93@hotmail.it>
2021-05-07 18:00:25 +0200

    wip

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = new Vue({
     data: {
         contacts: allContacts,
         activeContact: {},
-        newMessage: '',
+        newMessage: "",
+        searchText: "",
+        filteredContacts: []
 
     },
     methods: {
@@ -23,5 +25,30 @@ const app = new Vue({
         getImage(avatar) {
             return "../imgs/avatar" + avatar + ".jpg";
         },
+        onInput() {
+            console.log(this.searchText);
+
+            var result;
+            this.filteredContacts = []
+            // Filtro a partire dall'array contacts, come faccio? 
+            this.contacts.forEach((contact) => {
+                let contactName = contact.name.toLowerCase();
+                let searchedName = this.searchText.toLowerCase();
+                if (contactName.includes(searchedName)) {
+                    this.filteredContacts.push(contact)
+                }
+                //     // contact.visible = true;
+                // } else {
+                //     contact.visible = false;
+                // }
+                
+
+
+            });
+            console.log(this.filteredContacts);
+        },
+        mounted() {
+            this.filteredContacts = this.contacts;
+        }
     }
 })
